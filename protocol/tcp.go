dd459891fdde2eba552482f6011de9a325b82b18
Keyv Chan <keyvchan@gmail.com>
2022-04-26 11:12:06 +0800

    feat: send broadcast message to all connected client

--- protocol/tcp.go
+++ protocol/tcp.go
@@ -12,24 +12,38 @@ import (
 	"github.com/keyvchan/NetAssist/internal"
 )
 
+// store all connections in a slice
+var connections = map[net.Conn]bool{}
+
 func TCPServer() {
 	address := internal.GetArg(3)
 	listener, err := net.Listen("tcp", address)
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	// create a chennel to communicate between the read and write goroutines
+	message_chan := make(chan []byte)
+
+	// create a goroutine to read user input
+	go read_from_stdin(message_chan)
+	go write_to_conn(message_chan)
+
 	for {
 		conn, err := listener.Accept()
 		if err != nil {
 			log.Fatal(err)
 		}
 		log.Println("Accepted connection")
+		connections[conn] = true
 		go func() {
 			buf := make([]byte, 1024)
 			for {
 				_, err = conn.Read(buf)
 				if errors.Is(err, io.EOF) {
 					log.Println("Connection closed")
+					// remove from channel
+					delete(connections, conn)
 					break
 				}
 				if err != nil {
@@ -42,6 +56,28 @@ func TCPServer() {
 		}()
 	}
 }
+
+func write_to_conn(message_chan chan []byte) {
+	for {
+		message := <-message_chan
+		for conn := range connections {
+			conn.Write(message)
+		}
+	}
+
+}
+
+func read_from_stdin(message_chan chan []byte) {
+	for {
+		scanner := bufio.NewScanner(os.Stdin)
+		if scanner.Scan() {
+			message_chan <- scanner.Bytes()
+		} else {
+			log.Fatal(errors.New("failed to read from stdin"))
+		}
+	}
+}
+
 func TCPClient() {
 	address := internal.GetArg(3)
 	conn, err := net.Dial("tcp", address)
